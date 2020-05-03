40ad35bd746ea11509a4d651473ab1c1e54dcd3b
Furkan Yalçındağ <fyalcindag@oxityazilim.com>
2020-05-04 00:44:27 +0300

    booqe backend

--- booqe/models/Blog.py
+++ booqe/models/Blog.py
@@ -10,7 +10,7 @@ class Blog(models.Model):
     title = models.CharField(max_length=200)
     blog = models.TextField(blank=True)
     description = models.TextField(blank=True,null=True)
-    tags = models.CharField(max_length=1000)
+    tags = models.CharField(max_length=1000,blank=True,null=True)
     isPublish = models.BooleanField()
     creationDate = models.DateTimeField(auto_now_add=True, verbose_name='Kayıt Tarihi')
     modificationDate = models.DateTimeField(auto_now=True, verbose_name='Güncelleme Tarihi')
