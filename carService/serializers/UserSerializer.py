3b7ed809a8627808e407e53944940938cd0a73d6
furkanyalcindag <furkanyalcindag@gmail.com>
2020-10-01 07:43:24 +0300

    xcx

--- carService/serializers/UserSerializer.py
+++ carService/serializers/UserSerializer.py
@@ -18,7 +18,6 @@ class GroupSerializer(serializers.HyperlinkedModelSerializer):
 
 
 class UserAddSerializer(serializers.Serializer):
-
     uuid = serializers.UUIDField(read_only=True)
     user = UserSerializer(read_only=True)
     gender = serializers.CharField(required=False)
@@ -32,7 +31,7 @@ class UserAddSerializer(serializers.Serializer):
     mobilePhone = serializers.CharField(required=False)
 
     def create(self, validated_data):
-        
+
         user = User.objects.create_user(username=validated_data.get('username'),
                                         email=validated_data.get('username'))
         user.first_name = validated_data.get("first_name")
@@ -48,6 +47,57 @@ class UserAddSerializer(serializers.Serializer):
                                              , gender=validated_data.get('gender'))
             profile.mobilePhone = validated_data.get('mobile_phone')
             profile.address = validated_data.get('address')
+            profile.save()
+            return profile
+        except Exception:
+            user.delete()
+            raise serializers.ValidationError("lütfen tekrar deneyiniz")
+
+    def update(self, instance, validated_data):
+        pass
+
+
+class CustomerAddSerializer(serializers.Serializer):
+    uuid = serializers.UUIDField(read_only=True)
+    user = UserSerializer(read_only=True)
+    gender = serializers.CharField(required=False)
+    first_name = serializers.CharField(required=False)
+    last_name = serializers.CharField(required=False)
+    username = serializers.CharField(write_only=True, required=False,
+                                     validators=[UniqueValidator(queryset=User.objects.all())])
+    #password = serializers.CharField(write_only=True)
+    birthDate = serializers.DateField(required=False)
+    city = serializers.CharField(required=False)
+    mobilePhone = serializers.CharField(required=False)
+    isCorporate = serializers.BooleanField(required=True)
+    taxNumber = serializers.CharField(required=False)
+    firmName = serializers.CharField(required=False)
+
+    def create(self, validated_data):
+
+        user = User.objects.create_user(username=validated_data.get('username'),
+                                        email=validated_data.get('username'))
+        user.first_name = validated_data.get("first_name")
+        user.last_name = validated_data.get("last_name")
+        user.set_password("Servis123.")
+        user.save()
+
+        try:
+            group = Group.objects.get(name='Customer')
+            user.groups.add(group)
+            user.save()
+            profile = Profile.objects.create(user=user)
+            profile.mobilePhone = validated_data.get('mobile_phone')
+            profile.address = validated_data.get('address')
+
+            if validated_data.get('isCorporate'):
+                profile.taxNumber=validated_data.get('taxNumber')
+                profile.firmName=validated_data.get('firmName')
+                profile.isCorporate=True
+
+            else :
+                profile.isCorporate=False
+
             profile.save()
             return profile
         except Exception:
--- carService/urls.py
+++ carService/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 
-from carService import views
+from carService.Views.CustomerApiViews import CustomerApi
 from carService.Views.ProductApiViews import ProductApi
 from carService.Views.UserApiView import UserApi
 
@@ -9,9 +9,9 @@ app_name = 'carService'
 urlpatterns = [
 
     url(r'user-api/$', UserApi.as_view(), name='user-api'),
+    url(r'customer-api/', CustomerApi.as_view(), name='customer-api'),
     url(r'product-api/$', ProductApi.as_view(), name='product-api'),
 
-    url(r'swagger/$', views.schema_view, name='swagger'),
-
+    # url(r'swagger/$', views.schema_view, name='swagger'),
 
 ]
