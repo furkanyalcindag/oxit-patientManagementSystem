24041b5fc923c93e26938ad48e04f1ca6700db86
furkan.yalcindag@turkai.com <Furkan1905>
2021-02-08 17:31:57 +0300

    checking account list

--- carService/services/DashboardServices.py
+++ carService/services/DashboardServices.py
@@ -1,4 +1,8 @@
-from carService.models import Product, Car, Profile, Service, ServiceSituation, Situation
+import datetime
+
+from django.db.models import Q
+
+from carService.models import Product, Car, Profile, Service, ServiceSituation, Situation, PaymentMovement
 
 
 def get_product_count():
@@ -31,11 +35,26 @@ def get_waiting_approve_services_count():
     # x = ServiceSituation.objects.all().order_by('service', '-id').distinct('service').values()
     x = ServiceSituation.objects.order_by('service', '-id').distinct('service')
     return ServiceSituation.objects.filter(id__in=x).filter(
-        situation=Situation.objects.get(name__exact='Müşteri Onayı Bekleniyor'))
+        situation=Situation.objects.get(name__exact='Müşteri Onayı Bekleniyor')).count()
 
 
 def get_completed_services_count():
     # x = ServiceSituation.objects.all().order_by('service', '-id').distinct('service').values()
     x = ServiceSituation.objects.order_by('service', '-id').distinct('service')
     return ServiceSituation.objects.filter(id__in=x).filter(
-        situation=Situation.objects.get(name__exact='Müşteri Onayı Bekleniyor'))
+        situation=Situation.objects.get(Q(name__exact='Tamamlandı')) | Q(name__exact='TeslimEdildi')).count()
+
+
+def get_total_checking_account(type):
+    today = datetime.date.today()
+    today_min = datetime.datetime.combine(datetime.date.today(), datetime.time.min)
+    today_max = datetime.datetime.combine(datetime.date.today(), datetime.time.max)
+
+    if type=='monthly':
+
+        print(1)
+    elif type=='daily':
+        PaymentMovement.objects.filter(creationDate__range=(today_min,tod))
+        print(2)
+    elif type=='yearly':
+
