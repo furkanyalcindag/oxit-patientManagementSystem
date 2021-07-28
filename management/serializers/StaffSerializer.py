cc6c2f99ad937d5ccd71677668c8f405f33df992
akpiinar <senaa.akpinarr@gmail.com>
2021-07-28 15:10:21 +0300

    staff api edit

--- management/serializers/StaffSerializer.py
+++ management/serializers/StaffSerializer.py
@@ -7,7 +7,7 @@ from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
 from rest_framework.exceptions import ValidationError
 
-from management.serializers.GeneralSerializer import PageSerializer
+from management.serializers.GeneralSerializer import PageSerializer, SelectSerializer
 from pms.models import Profile
 
 
@@ -17,16 +17,23 @@ class StaffSerializer(serializers.Serializer):
     lastName = serializers.CharField()
     mobilePhone = serializers.CharField()
     email = serializers.CharField()
+    group = SelectSerializer(read_only=True)
+    groupId = serializers.IntegerField(write_only=True, allow_null=True)
 
     def update(self, instance, validated_data):
         try:
             with transaction.atomic():
 
                 user = instance.user
-                user.firstName = validated_data.get('firstName')
-                user.lastName = validated_data.get('lastName')
+                x = user.groups.filter(id=1)
+                user.first_name = validated_data.get('firstName')
+                user.last_name = validated_data.get('lastName')
                 user.email = validated_data.get('email')
                 user.username = validated_data.get('email')
+                user.groups.clear()
+                user.groups.add(Group.objects.get(id=validated_data.get('groupId')))
+                # x = Group.objects.get(id=validated_data.get('groupId'))
+                # user.groups.add(x)
                 user.save()
                 instance.mobilePhone = validated_data.get('mobilePhone')
                 instance.save()
@@ -46,8 +53,8 @@ class StaffSerializer(serializers.Serializer):
                 user.first_name = validated_data.get('firstName')
                 user.last_name = validated_data.get('lastName')
                 user.set_password('oxit2016')
-                group = Group.objects.get(name='Staff')
-                user.groups.add(group)
+                y = validated_data.get('groupId')
+                user.groups.add(y)
                 user.save()
                 profile = Profile.objects.create(user=user)
                 profile.mobilePhone = validated_data.get('mobilePhone')
@@ -70,10 +77,10 @@ class StaffSerializer(serializers.Serializer):
 
         else:
 
-            if isinstance(list(self.context)[0], str):
-                user = Profile.objects.get(uuid=list(self.context)[1].query_params['id']).user
-            else:
-                user = Profile.objects.get(uuid=list(self.context)[0].query_params['id']).user
+            # if isinstance(list(self.context)[0], str):
+            #     user = Profile.objects.get(uuid=list(self.context)[1].query_params['id']).user
+            # else:
+            #     user = Profile.objects.get(uuid=list(self.context)[0].query_params['id']).user
             if User.objects.filter(username=email).count() > 0:
                 raise serializers.ValidationError("Bu email sistemde kayıtlıdır")
         return email
